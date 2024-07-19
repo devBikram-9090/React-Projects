c10cacc73841997c46685571e258587f85e01189
devBikram-9090 <163228368+devBikram-9090@users.noreply.github.com>
2024-07-19 15:15:22 +1000

    Star rating component

---
+++ Project-6/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import StarRating from "./components/star-rating";
+
+function App() {
+  return (
+    <>
+      <StarRating noOfStars={10} />
+    </>
+  );
+}
+
+export default App;
